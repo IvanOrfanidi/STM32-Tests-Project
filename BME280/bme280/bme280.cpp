aedfa09817c930e4306b2076a5b8ec87c7daaa62
i.orfanidi <i.orfanidi@scout.local>
2018-12-28 13:25:43 +0300

    Пересоздал проект под правильное имя датчика BME280.

---
+++ BME280/bme280/bme280.cpp
@@ -0,0 +1,665 @@
+/**
+ ******************************************************************************
+ * @file    bme180.cpp
+ * @author  Ivan Orfanidi
+ * @version V1.0.0
+ * @date    10-April-2018
+ * @brief
+ ******************************************************************************
+ * @attention
+ *
+ *
+ * <h2><center>&copy; COPYRIGHT 2018 </center></h2>
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "bme280.hpp"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "board.hpp"
+
+
+/**
+ * @brief Статические экземпляры класса
+ */
+Bme* Bme::Bme280[Bme::BME280_MAX_COUNT];
+
+
+/**
+ * @brief Сonstructor
+ */
+Bme::Bme(I2C_TypeDef* i2c, uint32_t clock, const Params_t* const params)
+{
+   GPIO_InitTypeDef port;
+   port.GPIO_Mode = GPIO_Mode_AF_OD;
+   port.GPIO_Speed = GPIO_Speed_50MHz;
+
+   I2C_InitTypeDef init;
+   init.I2C_Mode = I2C_Mode_I2C;   // I2Cx mode is I2Cx
+   init.I2C_DutyCycle = I2C_DutyCycle_2;   // I2Cx fast mode duty cycle (WTF is this?)
+   init.I2C_OwnAddress1 = 1;   // This device address (7-bit or 10-bit)
+   init.I2C_Ack = I2C_Ack_Enable;   // Acknowledgment enable
+   init.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;   // choose 7-bit address for acknowledgment
+   init.I2C_ClockSpeed = clock;
+
+   I2Cx = nullptr;
+
+   if(i2c == I2C1)
+   {
+      if(Bme280[BME1]) {
+         return;
+      }
+      Bme280[BME1] = this;
+
+      /* Init Pins I2Cx */
+      RCC_APB2PeriphClockCmd(I2C1_PORT_CLOCK, ENABLE);
+      port.GPIO_Pin = I2C1_SDA_PIN | I2C1_SCL_PIN;
+
+      /* Enable I2Cx clock */
+      RCC_APB1PeriphClockCmd(I2C1_CLOCK, ENABLE);   // Enable I2Cx clock
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+   }
+   else if(i2c == I2C2)
+   {
+      if(Bme280[BME2]) {
+         return;
+      }
+      Bme280[BME2] = this;
+
+      /* Init Pins I2Cx */
+      RCC_APB2PeriphClockCmd(I2C2_PORT_CLOCK, ENABLE);
+      port.GPIO_Pin = I2C2_SDA_PIN | I2C2_SCL_PIN;
+
+      /* Enable I2Cx clock */
+      RCC_APB1PeriphClockCmd(I2C2_CLOCK, ENABLE);   // Enable I2Cx clock
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+   }
+   else {
+      return;
+   }
+
+   /* Init Pins GPIO */
+   GPIO_Init(I2C2_GPIO_PORT, &port);
+
+   I2Cx = i2c;
+
+   Clock = clock;
+
+   Address = BME280_ADDR;
+
+   /* Init I2Cx */
+   I2C_DeInit(I2Cx);        // De Init
+
+   I2C_Cmd(I2Cx, ENABLE);   // Enable I2Cx
+
+   I2C_Init(I2Cx, &init);   // Configure I2Cx
+
+   int timeout = TIMEOUT;
+   while(I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY)) {
+      timeout--;
+      if(!(timeout)) {
+         break;
+      }
+   }
+
+   Reset();
+
+   /* Get service information */
+   BmeInfo.ChipID = ReadReg(BME280_CHIP_ID_REG);
+   if(BmeInfo.ChipID == BME280_CHIP_ID)
+   {
+      // Если не задали параметры датчика, то подставляем дефолтные
+      if(params) {
+         memcpy(&Params, params, sizeof(Params_t));
+      }
+      else {
+         Params.Mode = BME280_MODE_NORMAL;
+         Params.Filter = BME280_FILTER_OFF;
+         Params.OversamplingTemperature = BME280_STANDARD;
+         Params.OversamplingHumidity = BME280_STANDARD;
+         Params.OversamplingPressure = BME280_STANDARD;
+         Params.Standby = BME280_STANDBY_250;
+      }
+   }
+   else {
+      // Датчик не читаеться
+      I2Cx = nullptr;
+   }
+}
+
+
+/**
+ * @brief Destructor
+ */
+Bme::~Bme()
+{
+   GPIO_InitTypeDef port;
+   port.GPIO_Mode = GPIO_Mode_AIN;
+   port.GPIO_Speed = GPIO_Speed_2MHz;
+
+   if(I2Cx == I2C1) {
+      if(Bme280[BME1] == nullptr) {
+         return;
+      }
+      Bme280[BME1] = nullptr;
+      port.GPIO_Pin = I2C1_SDA_PIN | I2C1_SCL_PIN;
+   }
+   else if(I2Cx == I2C2) {
+      if(Bme280[BME2] == nullptr) {
+         return;
+      }
+      Bme280[BME2] = nullptr;
+      port.GPIO_Pin = I2C2_SDA_PIN | I2C2_SCL_PIN;
+   }
+   else {
+      return;
+   }
+
+   GPIO_Init(I2C1_GPIO_PORT, &port);
+   I2C_DeInit(I2Cx);   // De Init
+}
+
+
+/**
+ * @brief Reset the I2C bus
+ */
+void Bme::ResetI2C()
+{
+   I2C_DeInit(I2Cx);   // De Init
+
+   I2C_InitTypeDef init;
+   init.I2C_Mode = I2C_Mode_I2C;   // I2Cx mode is I2Cx
+   init.I2C_DutyCycle = I2C_DutyCycle_2;   // I2Cx fast mode duty cycle (WTF is this?)
+   init.I2C_OwnAddress1 = 1;   // This device address (7-bit or 10-bit)
+   init.I2C_Ack = I2C_Ack_Enable;   // Acknowledgment enable
+   init.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;   // choose 7-bit address for acknowledgment
+   init.I2C_ClockSpeed = Clock;
+
+   I2C_Init(I2Cx, &init);   // Configure I2Cx
+}
+
+
+/**
+ * @brief Will perform the same sequence as power on reset
+ */
+void Bme::Reset()
+{
+   WriteReg(BME280_SOFT_RESET_REG, 0xB6);   // Do software reset
+   int timeout = TIMEOUT;
+   while(timeout--)
+   {
+      uint8_t status = ReadReg(BME280_STATUS);
+      if(!(status & 1)) {
+         break;
+      }
+   }
+}
+
+
+/**
+ * @brief Read calibration of coefficients
+ */
+bool Bme::ReadCalibration()
+{
+   Reset();
+
+   I2C_AcknowledgeConfig(I2Cx, ENABLE);   // Enable I2Cx acknowledge
+   I2C_GenerateSTART(I2Cx, ENABLE);   // Send START condition
+   int timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT)) {
+      timeout--;
+      if(!(timeout)) {
+         return false;
+      }
+   }
+   
+   I2C_Send7bitAddress(I2Cx, Address, I2C_Direction_Transmitter);   // Send slave address
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {   // Wait for EV6
+      timeout--;
+      if(!(timeout)) {
+         return false;
+      }
+   }
+   
+   I2C_SendData(I2Cx, BME280_CALIB);   // Send calibration first register address
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return false;
+      }
+   }
+   
+   I2C_GenerateSTART(I2Cx, ENABLE);   // Send repeated START condition (aka Re-START)
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT)) {   // Wait for EV5
+      timeout--;
+      if(!(timeout)) {
+         return false;
+      }
+   }
+   
+   I2C_Send7bitAddress(I2Cx, Address, I2C_Direction_Receiver);   // Send slave address for READ
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return false;
+      }
+   }
+
+   uint8_t buf[BME280_PROM_DATA_LEN];
+   for(auto i = 0; i < BME280_PROM_DATA_LEN - 1; i++)
+   {
+      timeout = TIMEOUT;
+      while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED)) {
+         timeout--;
+         if(!(timeout)) {
+            return false;
+         }
+      }
+      buf[i] = I2C_ReceiveData(I2Cx);   // Receive byte
+   }
+
+   I2C_AcknowledgeConfig(I2Cx, DISABLE);    // Disable I2Cx acknowledgment
+   I2C_GenerateSTOP(I2Cx, ENABLE);          // Send STOP condition
+   I2C_ReceiveData(I2Cx);                   // Receive last byte
+
+   /* Temperature */
+   Calibration.T1 = (uint16_t)((buf[1] << 8) | buf[0]);
+   Calibration.T2 = (int16_t)((buf[3] << 8) | buf[2]);
+   Calibration.T3 = (int16_t)((buf[5] << 8) | buf[4]);
+
+   /* Pressure */
+   Calibration.P1 = (uint16_t)((buf[7] << 8) | buf[6]);
+   Calibration.P2 = (int16_t)((buf[9] << 8) | buf[8]);
+   Calibration.P3 = (int16_t)((buf[11] << 8) | buf[10]);
+   Calibration.P4 = (int16_t)((buf[13] << 8) | buf[12]);
+   Calibration.P5 = (int16_t)((buf[15] << 8) | buf[14]);
+   Calibration.P6 = (int16_t)((buf[17] << 8) | buf[16]);
+   Calibration.P7 = (int16_t)((buf[19] << 8) | buf[18]);
+   Calibration.P8 = (int16_t)((buf[21] << 8) | buf[20]);
+   Calibration.P9 = (int16_t)((buf[23] << 8) | buf[22]);
+
+   /* Humidity */
+   Calibration.H1 = ReadReg(BME280_HUM_CALIB_H1);
+   Calibration.H2 = (int16_t)(ReadReg(BME280_HUM_CALIB_H2_LSB) << 8 | ReadReg(BME280_HUM_CALIB_H2_MSB));
+   Calibration.H3 = ReadReg(BME280_HUM_CALIB_H3);
+
+   const uint16_t h4 = ReadReg(BME280_HUM_CALIB_H4_LSB) << 8 | ReadReg(BME280_HUM_CALIB_H4_MSB);
+   const uint16_t h5 = ReadReg(BME280_HUM_CALIB_H5_LSB) << 8 | ReadReg(BME280_HUM_CALIB_H5_MSB);
+
+   Calibration.H4 = (int16_t)((h4 & 0x00ff) << 4 | (h4 & 0x0f00) >> 8);
+   Calibration.H5 = (int16_t)(h5 >> 4);
+
+   Calibration.H6 = (int8_t)ReadReg(BME280_HUM_CALIB_H6);
+
+   /* */
+   const uint8_t config = (Params.Standby << 5) | (Params.Filter << 2);
+   WriteReg(BME280_CONFIG, config);
+
+   /* */
+   WriteReg(BME280_CTRL_HUM, Params.OversamplingHumidity);
+
+   /* */
+   if(Params.Mode == BME280_MODE_FORCED) {
+      Params.Mode = BME280_MODE_SLEEP;   // initial mode for forced is sleep
+   }
+
+   const uint8_t ctrl = (Params.OversamplingTemperature << 5) | (Params.OversamplingPressure << 2) | (Params.Mode);
+   WriteReg(BME280_CTRL, ctrl);
+
+   return true;
+}
+
+/**
+ * @brief  Returns the status of class creation.
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::CreateClass() const
+{
+   return (I2Cx != nullptr);
+}
+
+
+/**
+ * @brief  Returns the real pressure in mmHg
+ * @param [out] pressure - the real pressure in Pa
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::GetPressureHg(uint16_t* const pressure)
+{
+    int32_t raw_pres;
+    if(ReadRawPressure(&raw_pres)) {
+        const uint32_t pa_press = CalcPressure(raw_pres);
+
+        /* Fast integer Pa -> mmHg conversion (Pascals to millimeters of mercury) */
+        *pressure = (pa_press * 75) / 10000;
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ * @brief  Returns the real pressure in Pa
+ * @param [out] pressure - the real pressure in Pa
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::GetPressurePa(uint32_t * const pressure)
+{
+    int32_t raw_pres;
+    if(ReadRawPressure(&raw_pres)) {
+        *pressure = CalcPressure(raw_pres);
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ * @brief  Returns the real humidity.
+ * @param [out] humidity - the real humidity
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::GetHumidity(float* const humidity)
+{
+    uint32_t raw_hum;
+    if(ReadRawHumidity(&raw_hum)) {
+        *humidity = CalcHumidity(raw_hum) / 1024;
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ * @brief  Get fine temperature
+ * @param [in] raw_pres - raw value
+ * @retval general value
+ */
+int32_t Bme::GetFineTemperature()
+{
+    int32_t raw_temp;
+    ReadRawTemperature(&raw_temp);
+    const int32_t var1 = ((((raw_temp >> 3) - ((int32_t)Calibration.T1 << 1))) * (int32_t)Calibration.T2) >> 11;
+
+    const int32_t var2 = (((((raw_temp >> 4) - (int32_t)Calibration.T1) * 
+                     ((raw_temp >> 4) - (int32_t)Calibration.T1)) >> 12) * (int32_t)Calibration.T3) >> 14;
+
+    return (var1 + var2);
+}
+
+
+/**
+ * @brief  Returns the real temperature.
+ * @param [out] temperature - the real temperature
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::GetTemperature(float* const temperature)
+{
+   int32_t raw_temp;
+   if(ReadRawTemperature(&raw_temp)) {
+      const float c_temp = CalcTemperature(raw_temp);
+      *temperature = c_temp / 100;
+      return true;
+   }
+   return false;
+}
+
+
+/**
+ * @brief  Returns the RAW humidity.
+ * @param [out] raw_pres - the RAW humidity
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::ReadRawHumidity(uint32_t* raw_hum)
+{
+    const uint8_t hum_msb = ReadReg(BME280_HUM_MSB);
+    const uint8_t hum_lsb = ReadReg(BME280_HUM_LSB);
+    
+    if((hum_msb == 0x80) && (hum_lsb == 0x00)) {
+        return false;
+    }
+    
+    *raw_hum = (uint32_t)((hum_msb << 8) | (hum_lsb));
+    return true;
+}
+
+
+/**
+ * @brief  Returns the RAW pressure.
+ * @param [out] raw_pres - the RAW pressure
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::ReadRawPressure(int32_t* raw_pres)
+{
+   const uint8_t pres_msb = ReadReg(BME280_PRESS_MSB);
+   const uint8_t pres_lsb = ReadReg(BME280_PRESS_LSB);
+   const uint8_t pres_xlsb = ReadReg(BME280_PRESS_XLSB);
+   
+    if((pres_msb == 0x80) && (pres_lsb == 0) && (pres_xlsb == 0)) {
+       return false;
+   }
+
+   *raw_pres = (int32_t)((pres_msb << 12) | (pres_lsb << 4) | (pres_xlsb >> 4));
+   return true;
+}
+
+
+/**
+ * @brief  Returns the RAW temperature.
+ * @param [out] raw_temp - the RAW temperature
+ * @retval true -  Success;
+ *         false - Fail.
+ */
+bool Bme::ReadRawTemperature(int32_t* raw_temp)
+{
+   const uint8_t temp_msb = ReadReg(BME280_TEMP_MSB);
+   const uint8_t temp_lsb = ReadReg(BME280_TEMP_LSB);
+   const uint8_t temp_xlsb = ReadReg(BME280_TEMP_XLSB);
+   
+   if((temp_msb == 0x80) && (temp_lsb == 0) && (temp_xlsb == 0)) {
+       return false;
+   }
+   
+   *raw_temp = (int32_t)((temp_msb << 12) | (temp_lsb << 4) | (temp_xlsb >> 4));
+   return true;
+}
+
+
+/**
+ * @brief  Calculating humidity
+ * @param [in] raw_hum - raw humidity
+ * @retval general value
+ */
+uint32_t Bme::CalcHumidity(uint32_t raw_hum)
+{
+    const int32_t fine_temp = GetFineTemperature();
+
+    int32_t v_x1 = fine_temp - (int32_t) 76800;
+    v_x1 = ((((raw_hum << 14) - ((int32_t) Calibration.H4 << 20)
+                - ((int32_t) Calibration.H5 * v_x1)) + (int32_t) 16384) >> 15)
+                * (((((((v_x1 * (int32_t) Calibration.H6) >> 10)
+                * (((v_x1 * (int32_t) Calibration.H3) >> 11)
+                + (int32_t) 32768)) >> 10) + (int32_t) 2097152)
+                * (int32_t) Calibration.H2 + 8192) >> 14);
+    
+    v_x1 = v_x1 - (((((v_x1 >> 15) * (v_x1 >> 15)) >> 7) * (int32_t) Calibration.H1) >> 4);
+    v_x1 = v_x1 < 0 ? 0 : v_x1;
+    v_x1 = v_x1 > 419430400 ? 419430400 : v_x1;
+    return v_x1 >> 12;
+}      
+
+
+/**
+ * @brief  Calculating pressure
+ * @param [in] raw_pres - raw value
+ * @retval general value
+ */
+uint32_t Bme::CalcPressure(int32_t raw_pres)
+{
+    const int32_t fine_temp = GetFineTemperature();
+    
+	int64_t var1 = (int64_t) fine_temp - 128000;
+	int64_t var2 = var1 * var1 * (int64_t) Calibration.P6;
+	var2 = var2 + ((var1 * (int64_t) Calibration.P5) << 17);
+	var2 = var2 + (((int64_t) Calibration.P4) << 35);
+	var1 = ((var1 * var1 * (int64_t) Calibration.P3) >> 8)
+			+ ((var1 * (int64_t) Calibration.P2) << 12);
+	var1 = (((int64_t) 1 << 47) + var1) * ((int64_t) Calibration.P1) >> 33;
+
+	if (var1 == 0) {
+		return 0;  // avoid exception caused by division by zero
+	}
+
+	int64_t p = 1048576 - raw_pres;
+	p = (((p << 31) - var2) * 3125) / var1;
+	var1 = ((int64_t) Calibration.P9 * (p >> 13) * (p >> 13)) >> 25;
+	var2 = ((int64_t) Calibration.P8 * p) >> 19;
+
+	p = ((p + var1 + var2) >> 8) + ((int64_t) Calibration.P7 << 4);
+	return p / 256;
+}
+
+
+/**
+ * @brief  Calculating temperature
+ * @param [in] raw_temp - raw value
+ * @retval general value
+ */
+int32_t Bme::CalcTemperature(int32_t raw_temp) const
+{
+   const int32_t var1 = ((((raw_temp >> 3) - ((int32_t)Calibration.T1 << 1))) * (int32_t)Calibration.T2) >> 11;
+
+   const int32_t var2 = (((((raw_temp >> 4) - (int32_t)Calibration.T1) * 
+                     ((raw_temp >> 4) - (int32_t)Calibration.T1)) >> 12) * (int32_t)Calibration.T3) >> 14;
+
+   int32_t fine_temp = var1 + var2;
+   return (fine_temp * 5 + 128) >> 8;
+}
+
+
+/**
+ * @brief  Write command
+ * @param [in] reg - register address
+ * @param [in] value - value data or command
+ * @retval return data from BME
+ */
+uint8_t Bme::WriteReg(uint8_t reg, uint8_t value)
+{
+   int timeout = TIMEOUT;
+   I2C_GenerateSTART(I2Cx, ENABLE);
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   I2C_Send7bitAddress(I2Cx, Address, I2C_Direction_Transmitter);   // Send slave address
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   I2C_SendData(I2Cx, reg);   // Send register address
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   I2C_SendData(I2Cx, value);   // Send register value
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   I2C_GenerateSTOP(I2Cx, ENABLE);
+   return value;
+}
+
+
+/**
+ * @brief  Read register
+ * @param [in] reg - register address
+ * @retval return data from BME
+ */
+uint8_t Bme::ReadReg(uint8_t reg)
+{
+   int timeout = TIMEOUT;
+   I2C_GenerateSTART(I2Cx, ENABLE);
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   I2C_Send7bitAddress(I2Cx, Address, I2C_Direction_Transmitter);   // Send slave address
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   I2C_SendData(I2Cx, reg);   // Send register address
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   I2C_GenerateSTART(I2Cx, ENABLE);   // Send repeated START condition (aka Re-START)
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   I2C_Send7bitAddress(I2Cx, Address, I2C_Direction_Receiver);   // Send slave address for READ
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   timeout = TIMEOUT;
+   while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED)) {
+      timeout--;
+      if(!(timeout)) {
+         return 0;
+      }
+   }
+   
+   const uint8_t value = I2C_ReceiveData(I2Cx);   // Receive
+   I2C_AcknowledgeConfig(I2Cx, DISABLE);   // Disable I2Cx acknowledgment
+   I2C_GenerateSTOP(I2Cx, ENABLE);   // Send STOP condition
+   return value;
+}
---
+++ BME280/bme280/bme280.hpp
@@ -0,0 +1,298 @@
+/**
+ ******************************************************************************
+ * @file    bme280.hpp
+ * @author  Ivan Orfanidi
+ * @version V1.0.0
+ * @date    28-March-2018
+ * @brief
+ ******************************************************************************
+ * @attention
+ *
+ *
+ ******************************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __BME280_HPP
+#define __BME280_HPP
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x_i2c.h"
+#include <stdint.h>
+#include <ctype.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define I2C1_PORT_CLOCK RCC_APB2Periph_GPIOB
+#define I2C1_SCL_PIN GPIO_Pin_6   // PB6
+#define I2C1_SDA_PIN GPIO_Pin_7   // PB7
+#define I2C1_GPIO_PORT GPIOB
+#define I2C1_CLOCK RCC_APB1Periph_I2C1
+
+#define I2C2_PORT_CLOCK RCC_APB2Periph_GPIOB
+#define I2C2_SCL_PIN GPIO_Pin_10   // PB10
+#define I2C2_SDA_PIN GPIO_Pin_11   // PB11
+#define I2C2_GPIO_PORT GPIOB
+#define I2C2_CLOCK RCC_APB1Periph_I2C2
+
+/**
+ * @brief Class sensor type BME280. Pressure and temperature.
+ */
+class Bme
+{
+ public:
+
+   /**
+    * Mode of BME280 module operation.
+    * Forced - Measurement is initiated by user.
+    * Normal - Continues measurement.
+    */
+   enum BmeMode_t
+   {
+      BME280_MODE_SLEEP = 0,
+      BME280_MODE_FORCED = 1,
+      BME280_MODE_NORMAL = 3,
+   };
+
+   //
+   enum BmeFilter_t
+   {
+      BME280_FILTER_OFF = 0,
+      BME280_FILTER_2 = 1,
+      BME280_FILTER_4 = 2,
+      BME280_FILTER_8 = 3,
+      BME280_FILTER_16 = 4
+   };
+
+   /**
+    * Pressure oversampling settings
+    */
+   enum BmeOversampling_t
+   {
+      BME280_SKIPPED = 0,           ///< no measurement
+      BME280_ULTRA_LOW_POWER = 1,   ///< oversampling x1
+      BME280_LOW_POWER = 2,         ///< oversampling x2
+      BME280_STANDARD = 3,          ///< oversampling x4
+      BME280_HIGH_RES = 4,          ///< oversampling x8
+      BME280_ULTRA_HIGH_RES = 5     ///< oversampling x16
+   };
+
+   /**
+    * Stand by time between measurements in normal mode
+    */
+   enum BmeStandbyTime_t
+   {
+      BME280_STANDBY_05 = 0,    ///< stand by time 0.5ms
+      BME280_STANDBY_62 = 1,    ///< stand by time 62.5ms
+      BME280_STANDBY_125 = 2,   ///< stand by time 125ms
+      BME280_STANDBY_250 = 3,   ///< stand by time 250ms
+      BME280_STANDBY_500 = 4,   ///< stand by time 500ms
+      BME280_STANDBY_1000 = 5,  ///< stand by time 1s
+      BME280_STANDBY_2000 = 6,  ///< stand by time 2s BME280, 10ms BME280
+      BME280_STANDBY_4000 = 7,  ///< stand by time 4s BME280, 20ms BME280
+   };
+
+   /**
+    * Configuration parameters for BME280 module.
+    * Use function bme280_init_default_params to use default configuration.
+    */
+   struct Params_t
+   {
+      BmeMode_t Mode;
+      BmeFilter_t Filter;
+      BmeOversampling_t OversamplingPressure;
+      BmeOversampling_t OversamplingTemperature;
+      BmeOversampling_t OversamplingHumidity;
+      BmeStandbyTime_t Standby;
+   };
+
+   /// Сonstructor
+   Bme(I2C_TypeDef*, uint32_t, const Params_t* const params = nullptr);
+
+   virtual ~Bme();   /// Destructor
+
+   bool GetPressurePa(uint32_t* const);     /// Get real pressure in Pa
+
+   bool GetPressureHg(uint16_t* const);     /// Get real pressure in Pa
+
+   bool GetTemperature(float* const);       /// Get real temperature
+
+   bool GetHumidity(float* const);          /// Get real humidity
+
+   bool CreateClass() const;    /// Returns the status of class creation
+
+   void Reset();                /// Reset sensor BME
+
+   void ResetI2C();             /// Reset the I2C bus
+
+   bool ReadCalibration();      /// Read calibration parameters
+
+ private:
+ 
+   /// BME280 defines
+   enum TimeoutDefines_t
+   {
+      TIMEOUT = 500
+   };
+
+   /**
+    * BME280 or BME280 address is 0x77 if SDO pin is high, and is 0x76 if
+    * SDO pin is low.
+    */
+   enum BmeDefines_t : uint8_t
+   {
+      BME280_ADDR = 0xEE,       ///< BME280 address
+      BME280_CHIP_ID = 0x60     ///< Chip-id. This value is fixed to 0x55.
+   };
+
+   /// Length
+   enum BmeLengthDefines_t
+   {
+      BME280_PROM_DATA_LEN = 24,   ///< E2PROM length
+   };
+
+   /// BME280 registers
+   enum BmeRegisters_t : uint8_t
+   {
+      BME280_PROM_START_ADDR = 0x88,    ///< E2PROM calibration data start register
+      BME280_CHIP_ID_REG = 0xD0,        ///< Chip ID
+      BME280_HUM_LSB = 0xFE,
+      BME280_HUM_MSB = 0xFD,
+      BME280_TEMP_XLSB = 0xFC,          ///< bits: 7-4
+      BME280_TEMP_LSB = 0xFB,
+      BME280_TEMP_MSB = 0xFA,
+      BME280_TEMP = BME280_TEMP_MSB,
+      BME280_PRESS_XLSB = 0xF9,         ///< bits: 7-4
+      BME280_PRESS_LSB = 0xF8,
+      BME280_PRESS_MSB = 0xF7,
+      BME280_PRESSURE = BME280_PRESS_MSB,
+      BME280_CONFIG = 0xF5,             ///< bits: 7-5 t_sb; 4-2 filter; 0 spi3w_en
+      BME280_CTRL = 0xF4,               ///< bits: 7-5 osrs_t; 4-2 osrs_p; 1-0 mode
+      BME280_STATUS = 0xF3,             ///< bits: 3 measuring; 0 im_update
+      BME280_CTRL_HUM = 0xF2,           ///< bits: 2-0 osrs_h;
+      BME280_CALIB = 0x88,              ///< E2PROM calibration data start register
+      BME280_HUM_CALIB_H1 = 0xA1,
+      BME280_HUM_CALIB_H2_LSB = 0xE1,
+      BME280_HUM_CALIB_H2_MSB = 0xE2,
+
+      BME280_HUM_CALIB_H3 = 0xE3,
+      BME280_HUM_CALIB_H4_MSB = 0xE4,
+      BME280_HUM_CALIB_H4_LSB = 0xE5,
+      BME280_HUM_CALIB_H5_MSB = 0xE5,
+      BME280_HUM_CALIB_H5_LSB = 0xE6,
+      BME280_HUM_CALIB_H6 = 0xE7,
+
+      BME280_SOFT_RESET_REG = 0xE0,   ///< Soft reset control
+
+   };
+
+   /// BME280 control values
+   enum BmeControlValues_t : uint8_t
+   {
+      BME280_T_MEASURE = 0x2E,   ///< temperature measurement
+      BME280_P0_MEASURE = 0x34,   ///< pressure measurement (OSS=0, 4.5ms)
+      BME280_P1_MEASURE = 0x74,   ///< pressure measurement (OSS=1, 7.5ms)
+      BME280_P2_MEASURE = 0xB4,   ///< pressure measurement (OSS=2, 13.5ms)
+      BME280_P3_MEASURE = 0xF4,   ///< pressure measurement (OSS=3, 25.5ms)
+   };
+
+   /// Calibration parameters structure
+   struct BmeCalibration_t
+   {
+      uint16_t T1;
+      int16_t T2;
+      int16_t T3;
+
+      uint16_t P1;
+      int16_t P2;
+      int16_t P3;
+      int16_t P4;
+      int16_t P5;
+      int16_t P6;
+      int16_t P7;
+      int16_t P8;
+      int16_t P9;
+
+      /* Humidity compensation for BME280 */
+      uint8_t H1;
+      int16_t H2;
+      uint8_t H3;
+      int16_t H4;
+      int16_t H5;
+      int8_t H6;
+
+      BmeCalibration_t()
+      {
+         T1 = 0;
+         T2 = 0;
+         T3 = 0;
+         P1 = 0;
+         P2 = 0;
+         P3 = 0;
+         P4 = 0;
+         P5 = 0;
+         P6 = 0;
+         P7 = 0;
+         P8 = 0;
+         P9 = 0;
+         H1 = 0;
+         H2 = 0;
+         H3 = 0;
+         H4 = 0;
+         H5 = 0;
+         H6 = 0;
+      }
+   };
+
+   /// Bme Information
+   struct BmeInformation_t
+   {
+      uint8_t ChipID;   ///< Chip ID. This value is fixed to 0x55
+   };
+
+   /// Count BME sensors
+   enum Bme_t
+   {
+      BME1,
+      BME2,
+
+      BME280_MAX_COUNT
+   };
+
+   bool ReadRawTemperature(int32_t*);
+   
+   bool ReadRawPressure(int32_t*);
+   
+   bool ReadRawHumidity(uint32_t*);
+   
+   int32_t GetFineTemperature();
+
+   int32_t CalcTemperature(int32_t) const;
+   
+   uint32_t CalcPressure(int32_t);
+   
+   uint32_t CalcHumidity(uint32_t);
+
+   uint8_t WriteReg(uint8_t, uint8_t);
+
+   uint8_t ReadReg(uint8_t);
+
+   BmeCalibration_t Calibration;   ///< Calibration parameters from E2PROM of BME280
+
+   BmeInformation_t BmeInfo;   ///< Service information about BME280
+
+   I2C_TypeDef* I2Cx;   ///< Указатель на экзепляр рабочей шины
+
+   Params_t Params;   ///< Параметры настроек датчика
+
+   uint8_t Address;   ///< Адрес датчика на шине
+
+   uint32_t Clock;   ///< Baud Rate шины I2C
+
+   static Bme* Bme280[BME280_MAX_COUNT];   ///< Статические экземпляры класса
+};
+
+#endif
