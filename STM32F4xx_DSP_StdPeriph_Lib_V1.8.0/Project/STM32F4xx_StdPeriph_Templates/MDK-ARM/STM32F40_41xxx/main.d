521f7b95344f170c79f9eb1d215b7b8b957058c3
i.orfanidi <i.orfanidi@scout.local>
2019-01-17 10:59:59 +0300

    Добавил библиотеку StdPeriph под stm32f4.

---
+++ STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Project/STM32F4xx_StdPeriph_Templates/MDK-ARM/STM32F40_41xxx/main.d
@@ -0,0 +1,10 @@
+stm32f40_41xxx\main.o: ../main.c
+stm32f40_41xxx\main.o: ../main.h
+stm32f40_41xxx\main.o: ../../../Libraries/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+stm32f40_41xxx\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm4.h
+stm32f40_41xxx\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32f40_41xxx\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+stm32f40_41xxx\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+stm32f40_41xxx\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+stm32f40_41xxx\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmSimd.h
+stm32f40_41xxx\main.o: ../../../Libraries/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
